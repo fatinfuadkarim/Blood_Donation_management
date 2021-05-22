d6d2223cef16331cb3428249a4ea42d591914ffe
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-05-22 22:22:49 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/main2.css">
+
+
+
+
+
+</head>
+<body>
+
+<div class="col-12" style="height: 350px;">
+	<div id="comname">
+		<i class="fa fa-balance-scale" aria-hidden="true"></i><br><br>BLOOD <b>DONATION</b>
+	</div>
+	<div id="nav" class="col-12">
+		<ul>
+		  <li><a href="index.php">Home</a></li>
+		  <li><a href="about.php">About</a></li>
+		  <li><a class="active" href="contact.php">Contact</a></li>
+		  <li><a href="register.php">Be A Donor</a></li>
+		  <li><a href="change_details.php">Change Details</a></li>
+		  <li><a href="find_blood.php">Find Donor</a></li>
+		  
+		</ul>
+	</div>
+	<span class="info2" style="left: 40%">CONTACT</span>
+	<img class="myFrontPic col-12" src="images/img11.jpg" style="height: 350px;">
+</div>
+<br>
+	
+		
+		
+		<br>
+		
+			<h1 align="center" style="color: rgb(76, 109, 86); font-size: 20px;margin: 5%;">CONTACT</h1>
+			
+			
+			<p align="center" style="color: rgb(76, 109, 86); font-size: 20px;margin: 5%;">Karim Blood Management</p>
+			<p align="center" style="color: rgb(76, 109, 86); font-size: 20px;margin: 5%;">01876058242</p>
+		</div>
+	</div>
+
+
+<div id="footer" class="col-12" style="margin: 0; padding: 0;overflow: auto;">
+	<?php include "footer.php"; ?>
+</div>
+
+</body>
+
+
+</html>
